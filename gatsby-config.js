b70a336ee4372445b49d1acb8e83c6e840392633
caren1 <czarnocki.w@wp.pl>
2021-01-01 21:47:28 +0100

    readme infographic

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: "GKW - Gdyńska klinika weterynaryjna",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`
+      }
+    }
+  ],
 };
